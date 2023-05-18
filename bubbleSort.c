e3d88f9a1e777fd3ad6a2b31bee7b360ec78d469
parkboomm <116609831+parkboomm@users.noreply.github.com>
2023-05-17 21:11:11 -0300

    Add files via upload

---
+++ bubbleSort.c
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubbleSort(int v[200], int n)
+{
+    int i, j, aux;
+    for(i = n-1; i > 0; i--){
+        for(j = 0; j < i; j++){
+            if(v[j] > v[j+1]){
+                aux = v[j]; v[j] = v[j+1]; v[j+1] = aux;
+            }
+        }
+    }
+}
+int main(){
+     int v[200], n ,i;
+        printf("Entre tamanho desejado do vetor:");
+        scanf("%d", &n);
+        printf("Entre os %d elementos do vetor:\n", n);
+        for(i = 0; i < n; i++){
+            scanf("%d", &v[i]);
+        }
+        bubbleSort(v, n);
+        printf("\n\nVetor ordenado:\n");
+        for(i = 0; i < n; i++)
+        {
+            printf("%d\t",v[i]);
+        }
+        printf("\n");
+        return 0;
+    
+}
